e351c1c105ec5a27a3dc34fac8917bf8a3acf589
sanghunlee <simoniful0@gmail.com>
2023-03-28 01:44:23 +0900

    Feat: 4장, ReservationAgency 객체 메서드 로직 수정 및 기존 로직 주석처리

--- Object.playground/Pages/Ch4_설계 품질과 트레이드오프.xcplaygroundpage/Sources/ReservationAgency/ReservationAgency.swift
+++ Object.playground/Pages/Ch4_설계 품질과 트레이드오프.xcplaygroundpage/Sources/ReservationAgency/ReservationAgency.swift
@@ -1,54 +1,59 @@
 import Foundation
 
 public class ReservationAgency {
-  public func reserve(
-    screening: Screening,
-    customer: Customer,
-    audienceCount: Int
-  ) -> Reservation {
-    let movie: Movie = screening.getMovie()
-    var discountable: Bool = false
-    
-    for condition in movie.getDiscountConditions() {
-      switch condition.getType() {
-      case .period:
-        let calendar = Calendar.current
-        let screeningWhenScreenedDate = calendar.date(from: screening.getWhenScreened())!
-        let screeningWhenScreenedWeekday = calendar.component(.weekday, from: screeningWhenScreenedDate)
-        let conditionWhenScreenedWeekday = condition.getDayOfWeek().rawValue
-        
-        let isSameWeekday = screeningWhenScreenedWeekday == conditionWhenScreenedWeekday
-        
-        let startTimeDate = calendar.date(from: condition.getStartTime())!
-        let endTimeDate = calendar.date(from: condition.getEndTime())!
-        
-        discountable = isSameWeekday && screeningWhenScreenedDate.isAvailableTime(from: startTimeDate, to: endTimeDate)
-      case .sequence:
-        discountable = condition.getSequence() == screening.getSequence()
-      }
-      
-      if discountable {
-        break
-      }
-    }
-    
-    var fee: Money
-    if discountable {
-      var discountAmount: Money = Money.ZERO
-      switch movie.getMovieType() {
-      case .amount:
-        discountAmount = movie.getDiscountAmount()
-      case .percent:
-        discountAmount = movie.getFee().times(percent: movie.getDiscountPercent())
-      case .none:
-        discountAmount = Money.ZERO
-      }
-      
-      fee = movie.getFee().minus(amount: discountAmount)
-    } else {
-      fee = movie.getFee()
-    }
-   
+//  public func reserve(
+//    screening: Screening,
+//    customer: Customer,
+//    audienceCount: Int
+//  ) -> Reservation {
+//    let movie: Movie = screening.getMovie()
+//    var discountable: Bool = false
+//
+//    for condition in movie.getDiscountConditions() {
+//      switch condition.getType() {
+//      case .period:
+//        let calendar = Calendar.current
+//        let screeningWhenScreenedDate = calendar.date(from: screening.getWhenScreened())!
+//        let screeningWhenScreenedWeekday = calendar.component(.weekday, from: screeningWhenScreenedDate)
+//        let conditionWhenScreenedWeekday = condition.getDayOfWeek().rawValue
+//
+//        let isSameWeekday = screeningWhenScreenedWeekday == conditionWhenScreenedWeekday
+//
+//        let startTimeDate = calendar.date(from: condition.getStartTime())!
+//        let endTimeDate = calendar.date(from: condition.getEndTime())!
+//
+//        discountable = isSameWeekday && screeningWhenScreenedDate.isAvailableTime(from: startTimeDate, to: endTimeDate)
+//      case .sequence:
+//        discountable = condition.getSequence() == screening.getSequence()
+//      }
+//
+//      if discountable {
+//        break
+//      }
+//    }
+//
+//    var fee: Money
+//    if discountable {
+//      var discountAmount: Money = Money.ZERO
+//      switch movie.getMovieType() {
+//      case .amount:
+//        discountAmount = movie.getDiscountAmount()
+//      case .percent:
+//        discountAmount = movie.getFee().times(percent: movie.getDiscountPercent())
+//      case .none:
+//        discountAmount = Money.ZERO
+//      }
+//
+//      fee = movie.getFee().minus(amount: discountAmount)
+//    } else {
+//      fee = movie.getFee()
+//    }
+//
+//    return Reservation(customer: customer, screening: screening, fee: fee, audienceCount: audienceCount)
+//  }
+  
+  func reserve(screening: Screening, customer: Customer, audienceCount: Int) -> Reservation {
+    let fee = screening.calculateFee(audienceCount: audienceCount)
     return Reservation(customer: customer, screening: screening, fee: fee, audienceCount: audienceCount)
   }
 }
